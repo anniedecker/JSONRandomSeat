ccd3c007cb5b8ee59496eca8b5a1e1f18ef22f8b
Annie Decker <anniedecker@ou.edu>
2019-10-30 11:20:40 -0500

    initial commit

---
+++ JSONRandomSeat/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSONRandomSeat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRollSheet_Click(object sender, RoutedEventArgs e)
+        {
+            //read in *only* JSON files
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "JSON files (*.json) | *.json";
+            var result = dlg.ShowDialog();
+            txtFile.Text = dlg.FileName;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(txtFile.Text) == true)
+            {
+
+                var lines = File.ReadAllLines(txtFile.Text);
+                List<int> seatNumbers = new List<int>();
+                Random random = new Random();
+                int row = 0;
+
+                for (int i =0; i<lines.Length; i++)
+                {
+                    int number;
+
+                    var line = lines[i];
+                    var column = line.Split(',');
+                    string name = column[0];
+
+                    //add in random number to assign seats, rows, and computer to various students
+                }
+                
+            }
+        }
+    }
+}
