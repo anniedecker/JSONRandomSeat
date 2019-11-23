4b2a9781b55770308b6bf310cc8a957f379ad854
Annie Decker <anniedecker@ou.edu>
2019-11-22 18:35:51 -0600

    cleaned up code

--- JSONRandomSeat/MainWindow.xaml.cs
+++ JSONRandomSeat/MainWindow.xaml.cs
@@ -31,8 +31,7 @@ namespace JSONRandomSeat
 
         private void btnRollSheet_Click(object sender, RoutedEventArgs e)
         {
-            //read in *only* JSON files
-            //students
+            //read in student JSON file
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.Filter = "JSON files (*.json) | *.json";
             var result = dlg.ShowDialog();
@@ -42,51 +41,13 @@ namespace JSONRandomSeat
 
         private void BtnWorkstation_Click(object sender, RoutedEventArgs e)
         {
-            //read in *only* JSON files
-            //workstations
+            //read in workstations JSON file
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.Filter = "JSON files (*.json) | *.json";
             var result = dlg.ShowDialog();
             txtFile2.Text = dlg.FileName;
         }
-
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (File.Exists(txtFile.Text) == true)
-        //    {
-
-        //        var lines = File.ReadAllLines(txtFile.Text);
-        //        //List<int> seatNumbers = new List<int>();
-        //        //Random random = new Random();
-        //        //int row = 0;
-
-        //        for (int i =0; i<lines.Length; i++)
-        //        {
-        //            int number;
-
-        //            var line = lines[i];
-        //            var column = line.Split(',');
-        //            string name = column[0];
-        //        }
-                
-        //    }
-            
-        //    if (File.Exists(txtFile2.Text) == true)
-        //    {
-
-        //        var lines = File.ReadAllLines(txtFile2.Text);
-        //         for (int i =0; i<lines.Length; i++)
-        //         {
-        //            int number;
-
-        //            var line = lines[i];
-        //            var column = line.Split(',');
-        //            string name = column[0];
-
-        //         }
-                
-        //    }
-        //}
+         
 
         private void txtFile_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -99,13 +60,10 @@ namespace JSONRandomSeat
             {
 
                 var lines = File.ReadAllLines(txtFile.Text);
-                //List<int> seatNumbers = new List<int>();
-                //Random random = new Random();
-                //int row = 0;
                 
                 for (int i = 0; i < lines.Length; i++)
                 {
-                    int number;
+                    //int number;
                     var line = lines[i];
                     var column = line.Split(',');
                     string name = column[0];
@@ -122,7 +80,7 @@ namespace JSONRandomSeat
                 var lines = File.ReadAllLines(txtFile2.Text);
                 for (int i = 0; i < lines.Length; i++)
                 {
-                    int number;
+                   // int number;
                     var line = lines[i];
                     var column = line.Split(',');
                     string seat = column[0];
